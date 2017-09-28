ace265b530f99f635b2f9af4991208b283009431
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2017-09-29 00:03:29 +0300

    rev3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+
+function ReverseStringFunc(str) {
+    var StrArr=[];
+    for(var i=0; i<str.length; i++){
+    StrArr[i]=str.charAt(i);
+    }
+    var RvrsStrArr=StrArr.reverse()
+    return RvrsStrArr.join("");
+}
+function ArrBlnkFunc(str1, str2){
+        var Arr=[];
+        for(var i=0; i<str2.length; i++) {
+            Arr.push([]);
+        for(var j=0 ; j<str1.length+str2.length; j++) {
+            Arr[i][j]=0;
+        }
+ }
+ return Arr;
+    }
+
 module.exports = function multiply(first, second) {
+    var ArrPrdct=[];
+    var FrstStr=ReverseStringFunc(first);
+    var SndStr=ReverseStringFunc(second);
+    var ArrBlnk=ArrBlnkFunc(first, second);
+  function MltplStrElemFunc(ArrB){
+      var Arr=ArrB;
+      for(var i=0; i<SndStr.length; i++) {
+           for(var j=0 ; j<FrstStr.length; j++) {
+            Arr[i][i+j]=SndStr.charAt(i)*FrstStr.charAt(j);// if time change data from str to array reverseString(change too)
+        }
+        }
+        return Arr;
+    }
+    var ArrPrdct = MltplStrElemFunc(ArrBlnk);
+ var NxtOrdr=0;
+ var FnshArr=[];
+for(var j=0; j<FrstStr.length+SndStr.length; j++){
+ FnshArr[j]=0;
+  }
+
+ for(var j=0; j<FrstStr.length+SndStr.length; j++){
+ for(var i=0; i<SndStr.length; i++){
+  FnshArr[j]=FnshArr[j]+ArrPrdct[i][j]+NxtOrdr;
+  NxtOrdr=0;
+   }
+   if(FnshArr[j]>10){
+       NxtOrdr=Math.floor(FnshArr[j]/10);
+       FnshArr[j]=FnshArr[j]%10;
+          }
+  }
+  FnshArr.reverse();
+  while(FnshArr[0]===0){
+      FnshArr.shift();
+
+  }
+  var FnshStr=FnshArr.join("");
+
+
+    return FnshStr;
   // your solution
 }
